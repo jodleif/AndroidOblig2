988f82a8cc024e533ed10e8fa53f9640eae804a7
Jo Øivind Gjernes <jodleif@gmail.com>
2017-04-17 14:30:42 +0200

    Fix settings menu

--- app/src/main/java/com/example/jo/obligatorisk2/SettingsActivity.java
+++ app/src/main/java/com/example/jo/obligatorisk2/SettingsActivity.java
@@ -4,6 +4,7 @@ package com.example.jo.obligatorisk2;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -20,21 +21,16 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
 import java.util.List;
 
 /**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
+ * Tilpasset "boilerplate" fra android-studio sin preference activity
+ * Satt opp med fragmenter som undermenyer for å illustrere muligheten
+ * for en mer omfattende oppsett med ulike kategorier av instillinger i appen
  */
 public class SettingsActivity extends AppCompatPreferenceActivity {
     /**
@@ -152,13 +148,30 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 || UserPreferenceFragment.class.getName().equals(fragmentName);
     }
 
-    public static class UserPreferenceFragment extends PreferenceFragment {
+    public static class UserPreferenceFragment extends PreferenceFragment
+    implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        public UserPreferenceFragment() {
+            super();
+        }
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_usersettings);
-            setHasOptionsMenu(true);
-            bindPreferenceSummaryToValue(findPreference("show_username"));
+            bindPreferenceSummaryToValue(findPreference("pref_username"));
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
         }
 
         @Override
@@ -168,6 +181,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 startActivity(new Intent(getActivity(),SettingsActivity.class));
             }
             return super.onOptionsItemSelected(item);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Log.d("[UserPreferenceFrag]", key);
         }
     }
 }
